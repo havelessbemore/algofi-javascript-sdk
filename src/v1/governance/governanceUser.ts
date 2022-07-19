07711c52ebdf14661c32e7ffdfc028a4739326d5
Armstrong Park <goarmstrongpark.ap@gmail.com>
2022-07-19 16:40:05 -0400

    added user state capability

--- src/v1/governance/governanceUser.ts
+++ src/v1/governance/governanceUser.ts
@@ -1,11 +1,15 @@
 import { Algodv2 } from "algosdk"
+import { getLocalStates } from "../stateUtils"
+import { parseAddressBytes } from "../utils"
 import GovernanceClient from "./governanceClient"
+import { ADMIN_STRINGS } from "./governanceConfig"
 import { UserAdminState, UserRewardsManagerState, UserVotingEscrowState } from "./userGovernanceStates"
 
 export default class governanceUser {
   public governanceClient: GovernanceClient
   public algod: Algodv2
   public address: string
+  public storageAddress: string
   public userVotingEscrowState: UserVotingEscrowState
   public userAdminState: UserAdminState
   public userRewardsManagerState: UserRewardsManagerState
@@ -16,12 +20,31 @@ export default class governanceUser {
     this.address = address
   }
 
-  async loadState(userLocalStates: {}) {
-    // TODO implement this
-    this.userVotingEscrowState = new UserVotingEscrowState()
-    // TODO implement this
-    this.userAdminState = new UserAdminState()
-    // TODO implement this
+  async loadState(userLocalStates: { [key: string]: {} }) {
+    for (const [key, value] of Object.entries(userLocalStates)) {
+      const appId = parseInt(key)
+      // Case when we have the local state of the user with the admin contract
+      if (appId == this.governanceClient.admin.adminAppId) {
+        // Get storage account
+        const userStorageAddress = parseAddressBytes(value[ADMIN_STRINGS.storage_account])
+        // Record storage account
+        this.storageAddress = userStorageAddress
+        // Get storage account local states
+        const userStorageLocalStates = await getLocalStates(this.algod, userStorageAddress)
+        // Get list of proposals to get state for
+        const proposals = Object.keys(this.governanceClient.admin.proposals)
+        this.userAdminState = new UserAdminState(
+          this.storageAddress,
+          userStorageLocalStates,
+          proposals,
+          this.governanceClient.admin.adminAppId
+        )
+      }
+      // Case when we have the local state of the user with the voting escrow contract
+      if (appId == this.governanceClient.votingEscrow.appId) {
+        this.userVotingEscrowState = new UserVotingEscrowState(value)
+      }
+    }
     this.userRewardsManagerState = new UserRewardsManagerState()
   }
 }
--- src/v1/governance/userGovernanceStates.ts
+++ src/v1/governance/userGovernanceStates.ts
@@ -1,3 +1,5 @@
+import { parseAddressBytes } from "../utils"
+import { ADMIN_STRINGS, PROPOSAL_STRINGS, VOTING_ESCROW_STRINGS } from "./governanceConfig"
 import Proposal from "./proposal"
 
 export class UserVotingEscrowState {
@@ -6,7 +8,13 @@ export class UserVotingEscrowState {
   public lockDuration: number
   public amountVeBank: number
   public boostMultiplier: number
-  constructor() {}
+  constructor(userLocalState: {}) {
+    this.amountLocked = userLocalState[VOTING_ESCROW_STRINGS.user_amount_locked]
+    this.lockStartTime = userLocalState[VOTING_ESCROW_STRINGS.user_lock_start_time]
+    this.lockDuration = userLocalState[VOTING_ESCROW_STRINGS.user_lock_duration]
+    this.amountVeBank = userLocalState[VOTING_ESCROW_STRINGS.user_amount_vebank]
+    this.boostMultiplier = userLocalState[VOTING_ESCROW_STRINGS.user_boost_multiplier]
+  }
 }
 
 export class UserAdminState {
@@ -14,8 +22,30 @@ export class UserAdminState {
   public openToDelegation: number
   public delegatorCount: number
   public delegatingTo: string
-  public userProposalStates: { [key: number]: UserProposalState }
-  constructor() {}
+  public userProposalStates: { [key: number]: UserProposalState } = {}
+  constructor(
+    storageAddress: string,
+    userStorageLocalStates: { [key: string]: {} },
+    proposalList: string[],
+    adminAppId: number
+  ) {
+    const proposalListNumbers = proposalList.map(proposal => parseInt(proposal))
+    this.storageAddress = storageAddress
+    // Loop through to get storage account's local state on admin
+    for (const [key, value] of Object.entries(userStorageLocalStates)) {
+      const appId = parseInt(key)
+      // Case when we have the storage account's local state with admin contract
+      if (appId == adminAppId) {
+        this.openToDelegation = value[ADMIN_STRINGS.open_to_delegation]
+        this.delegatorCount = value[ADMIN_STRINGS.delegator_count]
+        this.delegatingTo = value[ADMIN_STRINGS.delegating_to]
+      }
+      // If we have a proposal that the storage account is opted into
+      if (proposalListNumbers.includes(appId)) {
+        this.userProposalStates[appId] = new UserProposalState(value)
+      }
+    }
+  }
 }
 
 export class UserRewardsManagerState {}
@@ -23,5 +53,8 @@ export class UserRewardsManagerState {}
 export class UserProposalState {
   public forOrAgainst
   public votingAmount
-  constructor() {}
+  constructor(storageProposalLocalState: {}) {
+    this.forOrAgainst = storageProposalLocalState[PROPOSAL_STRINGS.for_or_against]
+    this.votingAmount = storageProposalLocalState[PROPOSAL_STRINGS.voting_amount]
+  }
 }
