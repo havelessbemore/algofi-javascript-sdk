c73cea2ad4dcb04b28eeef3ab50785bed296880b
Paul-Colella <paul.colella.a@gmail.com>
2022-07-22 21:09:59 +0000

    superagent

--- src/assetData/assetDataClient.ts
+++ src/assetData/assetDataClient.ts
@@ -2,7 +2,7 @@
 
 // external
 import { Algodv2 } from "algosdk"
-//import http from "http-browserify"
+import request from 'superagent';
 
 // global
 import AlgofiClient from "./../algofiClient"
@@ -33,45 +33,21 @@ export default class AssetDataClient {
 
   async loadState() {
     // load prices from amm analytics
-    //http.get(ANALYTICS_ENDPOINT + "/assets", (res) => {
-    //  const { statusCode } = res;
-    //  const contentType = res.headers['content-type'];
-    //  let error;
+    request
+       .get(ANALYTICS_ENDPOINT + "/assets")
+       .then(resp => {
+          if (resp.status == 200) {
+            for (const assetInfo of resp.body.assets) {
+              this.assets[assetInfo.asset_id] = new Asset(assetInfo.asset_id, assetInfo.name, assetInfo.decimals, assetInfo.price)
+            }
+          } else {
+            console.log("Bad Response")
+          }
+       })
+       .catch(err => {
+         console.log(err.message)
+       });
 
-    //  // Any 2xx status code signals a successful response but
-    //  // here we're only checking for 200.
-    //  if (statusCode !== 200) {
-    //    error = new Error('Request Failed.\n' +
-    //                      `Status Code: ${statusCode}`);
-    //  } else if (!/^application\/json/.test(contentType)) {
-    //    error = new Error('Invalid content-type.\n' +
-    //                      `Expected application/json but received ${contentType}`);
-    //  }
-
-    //  if (error) {
-    //    console.error(error.message);
-    //    // Consume response data to free up memory
-    //    res.resume();
-    //    return;
-    //  }
-
-    //  res.setEncoding('utf8');
-    //  let rawData = '';
-    //  res.on('data', (chunk) => { rawData += chunk; });
-    //  res.on('end', () => {
-    //    try {
-    //      const parsedData = JSON.parse(rawData);
-    //      for (const assetInfo of parsedData.body.assets) {
-    //        this.assets[assetInfo.asset_id] = new Asset(assetInfo.asset_id, assetInfo.name, assetInfo.decimals, assetInfo.price)
-    //      }
-    //    } catch (e) {
-    //      console.error(e.message);
-    //    }
-    //  });
-    //}).on('error', (e) => {
-    //  console.error(`Got error: ${e.message}`);
-    //});
-    
     // load prices from oracles (v2 lending)
     for (const [appId, market] of Object.entries(this.algofiClient.lending.v2.markets)) {
       // load underlying asset
