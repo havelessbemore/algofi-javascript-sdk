b78d966e847a12ac09ed9a535b737f84dcc77376
Paul-Colella <paul.colella.a@gmail.com>
2022-07-22 20:19:53 +0000

    skip http rew

--- src/assetData/assetDataClient.ts
+++ src/assetData/assetDataClient.ts
@@ -1,8 +1,8 @@
 // IMPORTS
-import http from "http"
 
 // external
 import { Algodv2 } from "algosdk"
+//import http from "http-browserify"
 
 // global
 import AlgofiClient from "./../algofiClient"
@@ -33,44 +33,44 @@ export default class AssetDataClient {
 
   async loadState() {
     // load prices from amm analytics
-    http.get(ANALYTICS_ENDPOINT + "/assets", (res) => {
-      const { statusCode } = res;
-      const contentType = res.headers['content-type'];
-      let error;
+    //http.get(ANALYTICS_ENDPOINT + "/assets", (res) => {
+    //  const { statusCode } = res;
+    //  const contentType = res.headers['content-type'];
+    //  let error;
 
-      // Any 2xx status code signals a successful response but
-      // here we're only checking for 200.
-      if (statusCode !== 200) {
-        error = new Error('Request Failed.\n' +
-                          `Status Code: ${statusCode}`);
-      } else if (!/^application\/json/.test(contentType)) {
-        error = new Error('Invalid content-type.\n' +
-                          `Expected application/json but received ${contentType}`);
-      }
+    //  // Any 2xx status code signals a successful response but
+    //  // here we're only checking for 200.
+    //  if (statusCode !== 200) {
+    //    error = new Error('Request Failed.\n' +
+    //                      `Status Code: ${statusCode}`);
+    //  } else if (!/^application\/json/.test(contentType)) {
+    //    error = new Error('Invalid content-type.\n' +
+    //                      `Expected application/json but received ${contentType}`);
+    //  }
 
-      if (error) {
-        console.error(error.message);
-        // Consume response data to free up memory
-        res.resume();
-        return;
-      }
+    //  if (error) {
+    //    console.error(error.message);
+    //    // Consume response data to free up memory
+    //    res.resume();
+    //    return;
+    //  }
 
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', (chunk) => { rawData += chunk; });
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-          for (const assetInfo of parsedData.body.assets) {
-            this.assets[assetInfo.asset_id] = new Asset(assetInfo.asset_id, assetInfo.name, assetInfo.decimals, assetInfo.price)
-          }
-        } catch (e) {
-          console.error(e.message);
-        }
-      });
-    }).on('error', (e) => {
-      console.error(`Got error: ${e.message}`);
-    });
+    //  res.setEncoding('utf8');
+    //  let rawData = '';
+    //  res.on('data', (chunk) => { rawData += chunk; });
+    //  res.on('end', () => {
+    //    try {
+    //      const parsedData = JSON.parse(rawData);
+    //      for (const assetInfo of parsedData.body.assets) {
+    //        this.assets[assetInfo.asset_id] = new Asset(assetInfo.asset_id, assetInfo.name, assetInfo.decimals, assetInfo.price)
+    //      }
+    //    } catch (e) {
+    //      console.error(e.message);
+    //    }
+    //  });
+    //}).on('error', (e) => {
+    //  console.error(`Got error: ${e.message}`);
+    //});
     
     // load prices from oracles (v2 lending)
     for (const [appId, market] of Object.entries(this.algofiClient.lending.v2.markets)) {
