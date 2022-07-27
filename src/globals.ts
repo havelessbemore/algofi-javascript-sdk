504faa69d7839b70ce7a14496c7f57a741344d3e
Paul-Colella <paul.colella.a@gmail.com>
2022-07-27 22:28:52 +0000

    get asset data backend working

--- src/globals.ts
+++ src/globals.ts
@@ -49,8 +49,7 @@ export const PERMISSIONLESS_SENDER_LOGIC_SIG = new LogicSigAccount(
 export const TEXT_ENCODER = new TextEncoder()
 export const TEXT_DECODER = new TextDecoder()
 
-export const ANALYTICS_ENDPOINT = "http://Algofianalytics-env.eba-nbpu8uqb.us-east-2.elasticbeanstalk.com"
-
+export const ANALYTICS_ENDPOINT = "https://api.algofi.org"
 // ENUMS
 
 export enum Network {
