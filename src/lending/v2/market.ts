9368f85de7d348c3d4f286de06a671d11fd7d001
Paul-Colella <paul.colella.a@gmail.com>
2022-07-27 20:58:06 +0000

    maybe value user states in market

--- src/lending/v2/market.ts
+++ src/lending/v2/market.ts
@@ -329,10 +329,10 @@ export default class Market {
     let maximumWithdrawUnderlying = this.getMaximumWithdrawAmount(user, borrowUtilLimit)
     let maximumWithdrawBAsset = Math.min(
       this.underlyingToBAsset(maximumWithdrawUnderlying).amount,
-      user.lending.v2.userMarketStates[this.appId].bAssetCollateral
+      user.lending.v2.userMarketStates[this.appId]?.bAssetCollateral || 0
     )
     if (user.lending.v2.netScaledBorrow == 0) {
-      maximumWithdrawBAsset = user.lending.v2.userMarketStates[this.appId].bAssetCollateral
+      maximumWithdrawBAsset = user.lending.v2.userMarketStates[this.appId]?.bAssetCollateral || 0
     }
     return this.assetDataClient.getAsset(maximumWithdrawBAsset, this.bAssetId)
   }
@@ -340,7 +340,7 @@ export default class Market {
   getMaximumBorrowAmount(user: AlgofiUser, borrowUtilLimit: number=0.9): AssetAmount {
     let userExcessScaledCollateral = user.lending.v2.netScaledCollateral * borrowUtilLimit - user.lending.v2.netScaledBorrow
     // special handling for initial borrow
-    if (user.lending.v2.userMarketStates[this.appId].borrowedAmount.amount == 0) {
+    if (user.lending.v2.userMarketStates[this.appId]?.borrowedAmount.amount || 0 == 0) {
       userExcessScaledCollateral -= 0.001
     }
     let maximumBorrowUSD = (userExcessScaledCollateral * FIXED_3_SCALE_FACTOR) / this.borrowFactor
@@ -351,11 +351,11 @@ export default class Market {
     let newUserScaledCollateral = user.lending.v2.netScaledCollateral + (collateralDelta.toUSD() * this.collateralFactor / FIXED_3_SCALE_FACTOR)
     let newUserScaledBorrow = (user.lending.v2.netScaledBorrow || 0) + (borrowDelta.toUSD() * this.borrowFactor / FIXED_3_SCALE_FACTOR)
     // special handling for initial borrow
-    if (borrowDelta.amount > 0 && user.lending.v2.userMarketStates[this.appId].borrowedAmount.amount == 0) {
+    if (borrowDelta.amount > 0 && user.lending.v2.userMarketStates[this.appId]?.borrowedAmount.amount || 0 == 0) {
       newUserScaledBorrow += 0.001
     }
     // special handling for final repay
-    if (borrowDelta.amount + user.lending.v2.userMarketStates[this.appId].borrowedAmount.amount <= 0) {
+    if (borrowDelta.amount + user.lending.v2.userMarketStates[this.appId]?.borrowedAmount.amount || 0 <= 0) {
       newUserScaledBorrow -= 0.001
     }
     if (newUserScaledBorrow > 0) {
